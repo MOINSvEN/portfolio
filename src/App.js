79a43822e95fd8f21ac17c0b9ccd087790c3a7cb
Sven Bastian <sven@MBP-von-Kenan.fritz.box>
2023-08-07 16:22:22 +0200

    mit sven

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React 1 2 3
-          salih hat auch mitgemacht
+          salih hat auch mitgemacht SVen auch
         </a>
       </header>
     </div>
